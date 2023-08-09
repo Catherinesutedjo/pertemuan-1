8e5e8dc676c777e6fe4d2e7dae56a14a7ddbf09d
mobdev <12345@gmAil.com>
2023-08-09 14:59:24 +0700

    init

---
+++ src/Learn.java
@@ -0,0 +1,11 @@
+public class Learn {
+    public static void main(String[] args) {
+
+        int X = 10;
+        int Y = 12;
+        int Z = X + Y;
+        System.out.println("Sum of X+Y= " + Z);
+    }
+
+
+}
---
+++ src/Pertemuan_1.java
@@ -0,0 +1,15 @@
+public class Pertemuan_1 {
+    public static void main(String[] args) {
+        System.out.println("----Data Mahasiswa---");
+        System.out.println("Nama : Catherine Sutedjo");
+        System.out.println("NRP : c14230087");
+        System.out.println("Mata Kuliah: Algoritma dan Pemograman");
+        System.out.println("Kelas : B");
+        System.out.println("Tahun : 2023/2024");
+        System.out.println("Semester : Gasal");
+        System.out.println("--------------------");
+
+
+    }
+}
+
